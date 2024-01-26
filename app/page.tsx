0319945c2789eb4a607747e36a6b3c8685fb64c9
Rodrigo Ramos <rodrigoror@gmail.com>
2024-01-26 20:42:32 -0300

    Update page.tsx
    inserindo H1

--- app/page.tsx
+++ app/page.tsx
@@ -104,7 +104,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            <H1>Instantly deploy your Next.js site to a shareable URL with Vercel.</H1>>
           </p>
         </a>
       </div>
